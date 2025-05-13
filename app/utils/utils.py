5f0dfdce1c258bbc83d1ef01fc407fdca26da8b7
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2025-05-13 13:12:51 -0300

    Actualizar la función get_ip_from_url para utilizar getaddrinfo de forma asíncrona y mejorar la resolución de dominios.

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlparse
 import socket
+import asyncio
 
 def get_domain_from_url(url: str) -> str:
     """
@@ -10,10 +11,17 @@ def get_domain_from_url(url: str) -> str:
 
 async def get_ip_from_url(domain: str) -> str:
     """
-    Dada una Dominio, devuelve su dirección IP.
+    Dada una Dominio, devuelve su dirección IP de forma asíncrona.
     """
     try:
-        ip_address = socket.gethostbyname(domain)
-        return ip_address
+        loop = asyncio.get_running_loop()
+        results = await loop.getaddrinfo(domain, None, family=socket.AF_UNSPEC, type=socket.SOCK_STREAM)
+        for res in results:
+            ip_address = res[4][0]
+            if ip_address:
+                return ip_address
+        raise ValueError(f"No se pudo encontrar una dirección IP en los resultados de getaddrinfo para el dominio {domain}")
+    except socket.gaierror as e:
+        raise ValueError(f"Error de resolución de DNS para el dominio {domain}: {str(e)}")
     except Exception as e:
         raise ValueError(f"No se pudo resolver la IP para el dominio {domain}: {str(e)}")
