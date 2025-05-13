03fe6061928e43b86cebfbbcdfc16bdf724bb239
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2025-05-13 13:13:17 -0300

    Agregar middleware CORS para permitir solicitudes desde todos los orígenes durante el desarrollo

--- app/main.py
+++ app/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
 from app.api.urls import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="SafeWaters API",
     description="API para detección de amenazas basada en consultas a servicios externos",
     version="1.0.0"
+)
+
+origins = [
+    "*" # Permite todos los orígenes (para desarrollo)
+    # "chrome-extension://<ID_DE_TU_EXTENSION>",
+    # "moz-extension://<ID_DE_TU_EXTENSION>"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"], # Permite todas las cabeceras
 )
 
 app.include_router(router)
