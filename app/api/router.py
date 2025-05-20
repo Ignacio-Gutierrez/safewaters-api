316f2335d36f85df9cc9a9cfe84ca28558a9572a
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2025-05-20 02:17:39 -0300

    Refactor el módulo de verificación de URLs: actualiza las importaciones y agrega la lógica de verificación en un nuevo archivo de servicio.

--- app/api/router.py
+++ app/api/router.py
@@ -1,19 +1,7 @@
 from fastapi import APIRouter
 
-# Importa el router de tu endpoint de verificación de URLs
 from .endpoints.urls_check import router as urls_check_router
-
-# Aquí importarás otros routers a medida que los crees
-# from .endpoints.auth import router as auth_router
-# from .endpoints.users import router as users_router
-# ... etc.
 
 api_router = APIRouter()
 
-# Incluye el router de verificación de URLs
 api_router.include_router(urls_check_router)
-
-# Incluye otros routers aquí
-# api_router.include_router(auth_router, prefix="/auth", tags=["Autenticación"])
-# api_router.include_router(users_router, prefix="/users", tags=["Usuarios"])
-# ... etc.
